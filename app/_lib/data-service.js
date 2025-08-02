18b44a502d9f6aaef54d1a341bd7c73398301783
Alyona Rogovaya <alyona@yelk.com.ua>
2025-08-02 19:13:18 +0200

    Added an error boundry and a not found page

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import { eachDayOfInterval } from "date-fns";
 import { supabase } from "./supabase";
 /////////////
@@ -12,6 +13,7 @@ export async function getCabin(id) {
 
   if (error) {
     console.error(error);
+    notFound();
   }
 
   return data;
