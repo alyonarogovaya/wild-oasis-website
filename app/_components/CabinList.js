67ed8bf962408536ce193dfb2876d24a99afff65
Alyona Rogovaya <alyona@yelk.com.ua>
2025-08-05 19:53:13 +0200

    Added a filter component to a cabins page

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,14 +1,28 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
 
-async function CabinList() {
+async function CabinList({ filter }) {
   const cabins = await getCabins();
 
   if (!cabins.length) return;
 
+  let filteredCabins;
+
+  if (filter === "all") {
+    filteredCabins = cabins;
+  } else if (filter === "small") {
+    filteredCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  } else if (filter === "medium") {
+    filteredCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+    );
+  } else if (filter === "large") {
+    filteredCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+  }
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {filteredCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
